3b5c7c12a21cd410cf262c1547a25ef99a9b745a
Deisel <74831846+Amelia-was@users.noreply.github.com>
2021-04-21 08:39:51 -0400

    contact form

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -106,8 +106,9 @@ function PhotoList({ category }) {
             <div className='flex-row'>
                 {currentPhotos.map((image, i) => (
 
+                    // default at the end ??
                     <img
-                        src={require(`../../assets/small/${category}/${i}.jpg`)}
+                        src={require(`../../assets/small/${category}/${i}.jpg`).default}
                         alt={image.name}
                         className='img-thumbnail mx-1'
                         key={image.name}
